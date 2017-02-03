86efce9344a95ab4f339c872e85a199f7568fc75
Golubkov Pavel <golubkov.p.d@gmail.com>
2017-02-03 20:32:00 +0500

    [Update] added plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-import webpack from 'webpack';
-import path from 'path';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+const webpack = require('webpack');
+const path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const AssetsPlugin = require('assets-webpack-plugin');
 
 const isProd = plugin => (process.env.NODE_ENV === 'production' ? plugin : undefined);
 
@@ -13,9 +15,15 @@ const cssName = process.env.NODE_ENV === 'production'
   : 'style.css';
 
 const plugins = [
-  new ExtractTextPlugin(cssName),
+  new ExtractTextPlugin(cssName, {
+    allChunks: true,
+  }),
   isProd(new webpack.optimize.DedupePlugin()),
   isProd(new webpack.optimize.OccurrenceOrderPlugin()),
+  isProd(new AssetsPlugin({
+    filename: 'assets.json',
+    path: path.join(__dirname, 'public', 'assets'),
+  })),
 ];
 
 const alias = {
@@ -23,6 +31,22 @@ const alias = {
   Api: path.resolve(__dirname, 'app/api'),
   Tools: path.resolve(__dirname, 'app/tools'),
 };
+
+const styleLoader = process.env.NODE_ENV !== 'production'
+  ? [
+    'style?sourceMap',
+    'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+    'scss',
+  ]
+  : ExtractTextPlugin.extract(
+    'style',
+    'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+    'scss',
+  );
+
+const jsLoader = process.env.NODE_ENV !== 'production'
+  ? 'babel!eslint-loader'
+  : 'babel';
 
 module.exports = {
   entry: ['./app/index.jsx'],
@@ -41,13 +65,11 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'scss-loader'),
+        loader: styleLoader,
       },
       {
-        test: /\.jsx$/,
-        loader: process.env.NODE_ENV !== 'production'
-          ? 'babel!eslint-loader'
-          : 'babel',
+        test: /\.(jsx|js)$/,
+        loader: jsLoader,
         exclude: [/node_modules/, /public/],
       },
     ],
