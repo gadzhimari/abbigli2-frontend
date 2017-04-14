6cdf60ac99e7e1d64991936630bd51c19c874605
Golubkov Pavel <golubkov.p.d@gmail.com>
2017-04-14 15:57:09 +0500

    added cookie parser, oauthHandler

--- server/server.js
+++ server/server.js
@@ -3,16 +3,21 @@ global.window = {};
 import express from 'express';
 import path from 'path';
 import compression from 'compression';
+import cookieParser from 'cookie-parser';
 
 import renderOnServer from './middlewares/renderOnServer';
+import oauthHandler from './middlewares/oauthHandling';
 
 const app = express();
 
-app.use(express.static('./public'));
 app.set('views', path.join(__dirname, '/templates'));
 app.set('view engine', 'ejs');
+app.use(express.static('./public'));
 
+app.use(cookieParser());
 app.use(compression());
+
+app.use(oauthHandler);
 app.use(renderOnServer);
 
 app.listen(3000, () => {
