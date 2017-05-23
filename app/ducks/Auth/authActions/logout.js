3f89729794e3698f76c3ab62ff6cd800264c7c9f
Голубков Павел <golubkov.p.d@gmail.com>
2017-05-23 12:21:02 +0500

    logout set cookie

--- app/ducks/Auth/authActions/logout.js
+++ app/ducks/Auth/authActions/logout.js
@@ -2,7 +2,7 @@ import { handleSucces } from './common';
 
 const logout = () => (dispatch) => {
   localStorage.removeItem('id_token');
-  document.cookie = 'id_token=';
+  document.cookie = 'id_token=; expires=-1';
 
   dispatch(handleSucces({
     isAuthenticated: false,
