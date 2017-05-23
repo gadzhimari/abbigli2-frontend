6f430532ef3d97ac3f6c5f44c6cee0ec172db7c1
Голубков Павел <golubkov.p.d@gmail.com>
2017-05-23 13:33:27 +0500

    seo fixes

--- app/components/Banner/Banner.js
+++ app/components/Banner/Banner.js
@@ -8,9 +8,9 @@ function Banner() {
     <div className="home-page__banner-wrap">
       <div className="home-page__banner">
         <div className="home-page__banner-text">
-          <h1 className="home-page__banner-heading">
+          <h2 className="home-page__banner-heading">
             {__t('banner.header')}
-          </h1>
+          </h2>
           {__t('banner.text')}
           <br/>
           <button className="button-banner">
