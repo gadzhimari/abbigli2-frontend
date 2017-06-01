ee4d48b618cabebf0d246ad544c7826211f20425
Голубков Павел <golubkov.p.d@gmail.com>
2017-06-01 16:43:13 +0500

    fixed date format and added labels

--- app/components/Popups/EventSearch/EventSearch.js
+++ app/components/Popups/EventSearch/EventSearch.js
@@ -1,17 +1,14 @@
 import React, { Component, PropTypes } from 'react';
-import DayPicker, { DateUtils } from 'react-day-picker';
-import moment from 'moment';
 
 import { SelectInput } from 'components';
 
+import moment from 'moment';
+
 import Popup from '../CommonPopup';
 
-import { API_URL, location } from 'config';
+import { API_URL } from 'config';
 import { __t } from '../../../i18n/translator';
 
-const formate = location === 'en'
-  ? 'dddd, MMMM Do YYYY'
-  : 'dddd, Do MMMM YYYY';
 
 class EventSearch extends Component {
   constructor(props) {
@@ -31,21 +28,16 @@ class EventSearch extends Component {
   doSearch = () => {
     const { options } = this.props;
     const { city, start, end } = this.state;
-    const startDay = moment(start).format('YYYY-MM-DD');
-    const endDay = moment(end).format('YYYY-MM-DD');
 
-    options.findEvents(city, startDay, endDay);
+    options.findEvents(city, start, end);
   }
 
   focusDate = ({ currentTarget }) => {
-    console.log(document.getElementById(currentTarget.dataset.name));
     document.getElementById(currentTarget.dataset.name).focus();
   }
 
   changeDate = ({ target }) => this.setState({
-    [target.dataset.name]: moment(target.value)
-      .locale(location)
-      .format(formate),
+    [target.dataset.name]: target.value,
   });
 
   render() {
@@ -62,13 +54,16 @@ class EventSearch extends Component {
             <div
               className="input-wrap input-date"
             >
+              <label className="popup-form__label" htmlFor="start">
+                {__t('Date.from')}
+              </label>
               <input
                 data-name="start"
+                id="start"
                 onChange={this.changeDate}
-                value={start || undefined}
+                value={start || moment(new Date()).format('YYYY-MM-DD')}
                 className="input events__search-input"
                 type="date"
-                placeholder={__t('Date.from')}
               />
             </div>
           </div>
@@ -76,15 +71,20 @@ class EventSearch extends Component {
             <div
               className="input-wrap input-date"
             >
+              <label className="popup-form__label" htmlFor="end">
+                {__t('Date.to')}
+              </label>
               <input
                 data-name="end"
-                value={end || undefined}
+                id="end"
+                value={end || moment(new Date()).format('YYYY-MM-DD')}
                 onChange={this.changeDate}
                 className="input input events__search-input"
                 type="date"
-                placeholder={__t('Date.to')}
               />
             </div>
+          </div>
+          <div className="popup-form__field">
             <div className="selectize-address">
               <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.6 18">
                 <path d="M6.3,0C2.817,0,0,2.816,0,6.3C0,11.025,6.3,18,6.3,18s6.3-6.975,6.3-11.7C12.6,2.816,9.785,0,6.3,0z M6.3,8.55 c-1.242,0-2.25-1.008-2.25-2.25S5.058,4.05,6.3,4.05c1.241,0,2.25,1.008,2.25,2.25S7.542,8.55,6.3,8.55z" />
