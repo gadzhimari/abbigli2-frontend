fedf9873fda0790ad99a135c81275a6e9392471b
Голубков Павел <golubkov.p.d@gmail.com>
2017-06-22 14:30:11 +0500

    nofollow links

--- app/components/SocialGroups/SocialGroups.js
+++ app/components/SocialGroups/SocialGroups.js
@@ -5,39 +5,39 @@ import { socialGroupsUrl, location } from 'config';
 
 const SocialGroups = () => (
   <div className="social-buttons">
-    <noindex><nofollow><ShareButton
+    <ShareButton
       className="social-btn"
       provider="facebook"
       link={socialGroupsUrl.fb}
-    /></nofollow></noindex>
-    <noindex><nofollow><ShareButton
+    />
+    <ShareButton
       className="social-btn"
       provider="pinterest"
       link={socialGroupsUrl.pinterest}
-    /></nofollow></noindex>
-    <noindex><nofollow><ShareButton
+    />
+    <ShareButton
       className="social-btn google-plus"
       provider="google"
       link={socialGroupsUrl.google}
-    /></nofollow></noindex>
+    />
 
     {
       location === 'ru'
       &&
-      <noindex><nofollow><ShareButton
+      <ShareButton
         className="social-btn vkontakte"
         provider="vk"
         link={socialGroupsUrl.vk}
-      /></nofollow></noindex>
+      />
     }
     {
       location === 'ru'
       &&
-      <noindex><nofollow><ShareButton
+      <ShareButton
         className="social-btn"
         provider="odnoklassniki"
         link={socialGroupsUrl.ok}
-      /></nofollow></noindex>
+      />
     }
   </div>
 );
