2f83cb75c94fe9bac047f1e39e335aef7ebd777a
Голубков Павел <golubkov.p.d@gmail.com>
2017-09-18 11:01:57 +0500

    #119 added ability to clear filter field by close icon

--- app/components/FiltersSelects/ChoiceColor/ChoiceColor.js
+++ app/components/FiltersSelects/ChoiceColor/ChoiceColor.js
@@ -59,9 +59,22 @@ class ChoiceColor extends PureComponent {
                 placeholder={__t('Pick a color')}
                 value={__t(activeColor)}
               />
-              <svg className="icon icon-arrow" viewBox="0 0 11.3 19.6">
-                <path d="M0.7,0.6c-0.8,0.8-0.9,2.1-0.1,2.9l5.9,6.4l-5.9,6.3c-0.8,0.9-0.8,2.2,0.1,2.9 c0.9,0.7,2.1,0.7,2.9-0.1l7.2-7.8c0.1-0.1,0.1-0.1,0.2-0.2l0,0c0.1-0.1,0.1-0.2,0.2-0.3l0,0c0-0.1,0.1-0.2,0.1-0.3v-0.1 c0-0.1,0-0.2,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.2,0-0.2V9.4c0-0.1-0.1-0.2-0.1-0.3l0,0C11.1,9,11.1,8.9,11,8.8 l0,0c-0.1-0.1-0.1-0.2-0.2-0.2L3.6,0.7C2.8-0.2,1.5-0.2,0.7,0.6z" />
-              </svg>
+              {
+                activeColor
+                  ? <svg
+                    className="icon icon-close"
+                    viewBox="0 0 14 14.031"
+                    onMouseDown={onChange}
+                    onTouchStart={onChange}
+                    data-value=""
+                    data-field="color"
+                  >
+                    <path d="M14,1.414L12.59,0L7,5.602L1.41,0L0,1.414l5.589,5.602L0,12.618l1.41,1.413L7,8.428l5.59,5.604L14,12.618 L8.409,7.016L14,1.414z"/>
+                  </svg>
+                  : <svg className="icon icon-arrow" viewBox="0 0 11.3 19.6">
+                    <path d="M0.7,0.6c-0.8,0.8-0.9,2.1-0.1,2.9l5.9,6.4l-5.9,6.3c-0.8,0.9-0.8,2.2,0.1,2.9 c0.9,0.7,2.1,0.7,2.9-0.1l7.2-7.8c0.1-0.1,0.1-0.1,0.2-0.2l0,0c0.1-0.1,0.1-0.2,0.2-0.3l0,0c0-0.1,0.1-0.2,0.1-0.3v-0.1 c0-0.1,0-0.2,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.2,0-0.2V9.4c0-0.1-0.1-0.2-0.1-0.3l0,0C11.1,9,11.1,8.9,11,8.8 l0,0c-0.1-0.1-0.1-0.2-0.2-0.2L3.6,0.7C2.8-0.2,1.5-0.2,0.7,0.6z" />
+                  </svg>
+              }
             </div>
         }
 
@@ -76,18 +89,6 @@ class ChoiceColor extends PureComponent {
                 onClick={onChange}
                 isActive={activeColor === color}
               />)
-            }
-            {
-              activeColor
-              &&
-              <a
-                className="filter__reset select__item"
-                onClick={onChange}
-                data-value=""
-                data-field="color"
-              >
-                {__t('Reset')}
-              </a>
             }
           </div>
         }
--- app/components/FiltersSelects/DateRange/DateRange.js
+++ app/components/FiltersSelects/DateRange/DateRange.js
@@ -13,12 +13,12 @@ class DateRange extends PureComponent {
     newTarget.dataset.field = newTarget.name;
 
     this.props.updateDate({
-      target: newTarget,
+      currentTarget: newTarget,
     });
   }
 
   render() {
-    const { dateStart, dateEnd, wrapperClass } = this.props;
+    const { dateStart, dateEnd, wrapperClass, updateDate } = this.props;
 
     return (
       <div className={wrapperClass}>
@@ -31,6 +31,20 @@ class DateRange extends PureComponent {
               value={dateStart}
               onChange={this.onChangeDate}
             />
+            {
+              dateStart
+              &&
+              <svg
+                className="icon icon-close"
+                viewBox="0 0 14 14.031"
+                onMouseDown={updateDate}
+                onTouchStart={updateDate}
+                data-value=""
+                data-field="date_start"
+              >
+                <path d="M14,1.414L12.59,0L7,5.602L1.41,0L0,1.414l5.589,5.602L0,12.618l1.41,1.413L7,8.428l5.59,5.604L14,12.618 L8.409,7.016L14,1.414z" />
+              </svg>
+            }
           </div>
         </div >
         <div className="calendar input-wrap">
@@ -42,6 +56,20 @@ class DateRange extends PureComponent {
               value={dateEnd}
               onChange={this.onChangeDate}
             />
+            {
+              dateEnd
+              &&
+              <svg
+                className="icon icon-close"
+                viewBox="0 0 14 14.031"
+                onMouseDown={updateDate}
+                onTouchStart={updateDate}
+                data-value=""
+                data-field="date_end"
+              >
+                <path d="M14,1.414L12.59,0L7,5.602L1.41,0L0,1.414l5.589,5.602L0,12.618l1.41,1.413L7,8.428l5.59,5.604L14,12.618 L8.409,7.016L14,1.414z" />
+              </svg>
+            }
           </div>
         </div >
       </div>
