f65c9211f66a3d4eb1675cc64b2ebcfe356a9931
Голубков Павел <golubkov.p.d@gmail.com>
2017-09-20 10:54:33 +0500

    thumbs-url: fixed url for thumb images

--- app/components/OtherArticles/OtherArticles.js
+++ app/components/OtherArticles/OtherArticles.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
-import { DOMAIN_URL } from 'config';
+
+import { THUMBS_URL } from 'config';
+import { __t } from '../../i18n/translator';
+
 
 import './OtherArticles.less';
 
@@ -10,36 +13,36 @@ const urls = {
   4: 'blog',
 };
 
-const OtherArticles = ({
-  articles,
-}) => {
-  return (
-    <div className="other-articles">
-      <a className="other-articles__item">
-        <span className="other-articles__item-text">
-          Другие статьи автора
-        </span>
-      </a>
-      {
-        articles
-        &&
-        articles
-          .slice(0, 4)
-          .map(article => (
-            <Link
-              className="other-articles__item"
-              key={article.id}
-              to={`/${urls[article.type]}/${article.slug}`}
-            >
-              <img
-                src={`${DOMAIN_URL}thumbs/unsafe/120x103/${article.images[0].file}`}
-                alt={article.title}
-              />
-            </Link>
-          ))
-      }
-    </div>
-  );
+const OtherArticles = ({ articles }) => (
+  <div className="other-articles">
+    <a className="other-articles__item">
+      <span className="other-articles__item-text">
+        {__t("Other autor's posts")}
+      </span>
+    </a>
+    {
+      articles
+      &&
+      articles
+        .slice(0, 4)
+        .map(article => (
+          <Link
+            className="other-articles__item"
+            key={article.id}
+            to={`/${urls[article.type]}/${article.slug}`}
+          >
+            <img
+              src={`${THUMBS_URL}unsafe/120x103/${article.images[0].file}`}
+              alt={article.title}
+            />
+          </Link>
+        ))
+    }
+  </div>
+);
+
+OtherArticles.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default OtherArticles;
