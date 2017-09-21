4e023cdbcb5705fd91aada532248ad60dfd75239
Голубков Павел <golubkov.p.d@gmail.com>
2017-09-21 10:32:52 +0500

    #138 Other-articles: fixed label and transitions for products

--- app/components/OtherArticles/OtherArticles.js
+++ app/components/OtherArticles/OtherArticles.js
@@ -11,35 +11,42 @@ import './OtherArticles.less';
 const urls = {
   3: 'event',
   4: 'blog',
+  1: 'post',
 };
 
-const OtherArticles = ({ articles }) => (
-  <div className="other-articles">
-    <a className="other-articles__item">
-      <span className="other-articles__item-text">
-        {__t("Other autor's posts")}
-      </span>
-    </a>
-    {
-      articles
-      &&
-      articles
-        .slice(0, 4)
-        .map(article => (
-          <Link
-            className="other-articles__item"
-            key={article.id}
-            to={`/${urls[article.type]}/${article.slug}`}
-          >
-            <img
-              src={`${THUMBS_URL}unsafe/120x103/${article.images[0].file}`}
-              alt={article.title}
-            />
-          </Link>
-        ))
-    }
-  </div>
-);
+const OtherArticles = ({ articles }) => {
+  if (!articles || articles.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="other-articles">
+      <a className="other-articles__item">
+        <span className="other-articles__item-text">
+          {__t("More from author")}
+        </span>
+      </a>
+      {
+        articles
+        &&
+        articles
+          .slice(0, 4)
+          .map(article => (
+            <Link
+              className="other-articles__item"
+              key={article.id}
+              to={`/${urls[article.type]}/${article.slug}`}
+            >
+              <img
+                src={`${THUMBS_URL}unsafe/120x103/${article.images[0].file}`}
+                alt={article.title}
+              />
+            </Link>
+          ))
+      }
+    </div>
+  );
+};
 
 OtherArticles.propTypes = {
   articles: PropTypes.arrayOf(PropTypes.object).isRequired,
