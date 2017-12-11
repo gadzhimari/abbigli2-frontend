567895afd2ed68b267c8e72a6141e77fb0bf80b3
Голубков Павел <golubkov.p.d@gmail.com>
2017-12-11 21:52:55 +0500

    add redis to config

--- server/middlewares/redis-client.js
+++ server/middlewares/redis-client.js
@@ -1,7 +1,11 @@
 import redis from 'redis';
 
+console.log(process.env.REDIS_HOST, process.env.REDIS_PORT);
+
 const client = redis.createClient({
   db: 1,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 });
 
 client.on('connect', () => {
