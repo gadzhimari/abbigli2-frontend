61d51ffc524afa74a573bdb4dcc05eabe20836aa
Голубков Павел <golubkov.p.d@gmail.com>
2017-12-24 21:04:21 +0500

    Добавил загрузочные стили

--- server/middlewares/set-locals.js
+++ server/middlewares/set-locals.js
@@ -1,13 +1,27 @@
+import fs from 'fs';
+import path from 'path';
 import { assetsUrl, getAssets } from '../lib/etc/assets';
 import metriks from '../lib/etc/metriks';
 
 const lang = process.env.LOCATION;
+const assets = getAssets();
+let criticalCss = '';
 
+if (process.env.NODE_ENV === 'production') {
+  criticalCss = fs.readFileSync(path.resolve(__dirname, `../../public${assets.css}`));
+}
+
+/**
+ * Промежуточный обработчик для установки переменных
+ * которые можно использовать в шаблонах
+ * пример использования в templates/index.ejs
+ */
 function setLocals(req, res, next) {
-  res.locals.assets = getAssets();
+  res.locals.assets = assets;
   res.locals.assetsUrl = assetsUrl;
   res.locals.metriks = metriks[lang];
   res.locals.lang = lang;
+  res.locals.criticalCss = criticalCss;
 
   next();
 }
