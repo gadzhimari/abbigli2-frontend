0d3f2c01a8464f1d0fa4eb28d784714e9472d85f
Голубков Павел <golubkov.p.d@gmail.com>
2017-12-24 21:30:34 +0500

    Немного улучшил код

--- server/middlewares/trim-url-slash.js
+++ server/middlewares/trim-url-slash.js
@@ -1,9 +1,9 @@
 export default function trimSlash(req, res, next) {
-  const slashOnEndWithSymbolRegexp = /.\/$/;
-  const slashOnEndRegexp = /\/$/;
+  const slashAtEndWith = /(.)\/$/;
 
-  if (slashOnEndWithSymbolRegexp.test(req.path)) {
-    const pathWithoutSlash = req.path.replace(slashOnEndRegexp, '');
+
+  if (slashAtEndWith.test(req.path)) {
+    const pathWithoutSlash = req.path.replace(slashAtEndWith, '$1');
     const pathToRedirect = req.originalUrl.replace(req.path, pathWithoutSlash);
 
     res.redirect(301, pathToRedirect);
