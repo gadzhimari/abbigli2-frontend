d771b74e891f146b57167217bff9a9734271e810
Alex <alexnofoget@mail.ru>
2018-01-28 01:04:38 +0300

    add edit button to post

--- app/containers/BlogPage/BlogPage.js
+++ app/containers/BlogPage/BlogPage.js
@@ -162,6 +162,13 @@ class BlogPage extends Component {
 
             <div>{processBlogContent(data.content)}</div>
 
+            <button className="edit-btn" type="button">
+              <svg className="icon icon-edit" viewBox="0 0 18 18">
+                <path d="M0,14.249V18h3.75L14.807,6.941l-3.75-3.749L0,14.249z M17.707,4.042c0.391-0.391,0.391-1.02,0-1.409l-2.34-2.34c-0.391-0.391-1.019-0.391-1.408,0l-1.83,1.829l3.749,3.749L17.707,4.042z" />
+              </svg>
+              {__t('Edit')}
+            </button>
+
             <FavoriteAdd
               toggleFavorite={this.handleFavorite}
               isFavorited={data.favorite}
