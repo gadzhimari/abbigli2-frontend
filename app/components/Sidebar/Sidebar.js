98573bc069b8bd1a76eccdb61d124111636cc896
Gadzhimari Kachaev <gadzhimari@gmail.com>
2018-02-19 19:48:54 +0300

    Hide popular in blogs and events if items is equal to zero

--- app/components/Sidebar/Sidebar.js
+++ app/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import { FavoriteAdd, Share } from 'components';
@@ -14,49 +14,55 @@ const urls = {
   3: 'event',
 };
 
-const Sidebar = ({
-  data,
-  newPosts,
-  popularPosts,
-  isFavorited,
-  toggleFavorite,
-  seeAllUrl,
-  newSectionTitle,
-  popularSectionTitle,
-}) => (
-    <div className="sidebar">
-      <div className="sidebar__group sidebar__group_favourite">
-        <FavoriteAdd
-          toggleFavorite={toggleFavorite}
-          isFavorited={isFavorited}
-        />
-      </div>
-      <TagsList
-        tags={data.tags}
-        type={data.type}
-      />
-      <div className="sidebar__group sidebar__group_social">
-        <Share
-          buttonClass="social-btn"
-          postLink={`/${urls[data.type]}/${data.slug}`}
-        />
-      </div>
-      <SidebarList
-        items={newPosts}
-        title={newSectionTitle}
-        seeAllUrl={seeAllUrl}
-      />
-      <SidebarList
-        items={popularPosts}
-        title={__t('Popular in blogs')}
-        seeAllUrl={`${seeAllUrl}?popular=true`}
-      />
-    </div>
-  );
+class Sidebar extends PureComponent {
+  static propTypes = {
+    isFavorited: PropTypes.bool.isRequired,
+    toggleFavorite: PropTypes.func.isRequired,
+  };
 
-Sidebar.propTypes = {
-  isFavorited: PropTypes.bool.isRequired,
-  toggleFavorite: PropTypes.func.isRequired,
-};
+  render() {
+    const {
+      data,
+      newPosts,
+      popularPosts,
+      isFavorited,
+      toggleFavorite,
+      seeAllUrl,
+      newSectionTitle,
+      popularSectionTitle,
+    } = this.props;
+
+    return (
+      <div className="sidebar">
+        <div className="sidebar__group sidebar__group_favourite">
+          <FavoriteAdd
+            toggleFavorite={toggleFavorite}
+            isFavorited={isFavorited}
+          />
+        </div>
+        <TagsList
+          tags={data.tags}
+          type={data.type}
+        />
+        <div className="sidebar__group sidebar__group_social">
+          <Share
+            buttonClass="social-btn"
+            postLink={`/${urls[data.type]}/${data.slug}`}
+          />
+        </div>
+        <SidebarList
+          items={newPosts}
+          title={newSectionTitle}
+          seeAllUrl={seeAllUrl}
+        />
+        <SidebarList
+          items={popularPosts}
+          title={popularSectionTitle}
+          seeAllUrl={`${seeAllUrl}?popular=true`}
+        />
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
--- app/components/Sidebar/SidebarList.js
+++ app/components/Sidebar/SidebarList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import moment from 'moment';
@@ -11,57 +11,60 @@ const urls = {
   3: 'event',
 };
 
-const SidebarList = ({
-  items,
-  title,
-  seeAllUrl,
-}) => (
-    <div className="sidebar__group">
-      <div className="sidebar__title">
-        {title}
-      </div>
-      <div className="sidebar-news">
-        {
-          items
-            .slice(0, 4)
-            .map(item => (
-              <div key={item.id} className="sidebar-news__item">
-                <Link
-                  className="sidebar-news__img"
-                  to={`/${urls[item.type]}/${item.slug}`}
-                >
-                  <img
-                    src={`${THUMBS_URL}unsafe/120x103/${item.images[0].file}`}
-                    alt={item.title}
-                  />
-                </Link>
-                <Link
-                  className="sidebar-news__title"
-                  to={`/${urls[item.type]}/${item.slug}`}
-                >
-                  {item.title}
-                </Link>
-                <div className="sidebar-news__date">
-                  {
-                    moment(item.created)
-                      .locale(location)
-                      .format('LL')
-                  }
+class SidebarList extends PureComponent {
+  render() {
+    const { items, title, seeAllUrl } = this.props;
+
+    return (
+      items.length > 0 &&
+      <div className="sidebar__group">
+        <div className="sidebar__title">
+          {title}
+        </div>
+        <div className="sidebar-news">
+          {
+            items
+              .slice(0, 4)
+              .map(item => (
+                <div key={item.id} className="sidebar-news__item">
+                  <Link
+                    className="sidebar-news__img"
+                    to={`/${urls[item.type]}/${item.slug}`}
+                  >
+                    <img
+                      src={`${THUMBS_URL}unsafe/120x103/${item.images[0].file}`}
+                      alt={item.title}
+                    />
+                  </Link>
+                  <Link
+                    className="sidebar-news__title"
+                    to={`/${urls[item.type]}/${item.slug}`}
+                  >
+                    {item.title}
+                  </Link>
+                  <div className="sidebar-news__date">
+                    {
+                      moment(item.created)
+                        .locale(location)
+                        .format('LL')
+                    }
+                  </div>
                 </div>
-              </div>
-            ))
-        }
-        <Link
-          className="show-more"
-          to={seeAllUrl}
-        >
-          {__t('See all')}
-          <svg className="icon icon-arrow" viewBox="0 0 11.3 19.6">
-            <path d="M0.7,0.6c-0.8,0.8-0.9,2.1-0.1,2.9l5.9,6.4l-5.9,6.3c-0.8,0.9-0.8,2.2,0.1,2.9 c0.9,0.7,2.1,0.7,2.9-0.1l7.2-7.8c0.1-0.1,0.1-0.1,0.2-0.2l0,0c0.1-0.1,0.1-0.2,0.2-0.3l0,0c0-0.1,0.1-0.2,0.1-0.3v-0.1 c0-0.1,0-0.2,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.2,0-0.2V9.4c0-0.1-0.1-0.2-0.1-0.3l0,0C11.1,9,11.1,8.9,11,8.8 l0,0c-0.1-0.1-0.1-0.2-0.2-0.2L3.6,0.7C2.8-0.2,1.5-0.2,0.7,0.6z" />
-          </svg>
-        </Link>
+              ))
+          }
+          <Link
+            className="show-more"
+            to={seeAllUrl}
+          >
+            {__t('See all')}
+            <svg className="icon icon-arrow" viewBox="0 0 11.3 19.6">
+              <path d="M0.7,0.6c-0.8,0.8-0.9,2.1-0.1,2.9l5.9,6.4l-5.9,6.3c-0.8,0.9-0.8,2.2,0.1,2.9 c0.9,0.7,2.1,0.7,2.9-0.1l7.2-7.8c0.1-0.1,0.1-0.1,0.2-0.2l0,0c0.1-0.1,0.1-0.2,0.2-0.3l0,0c0-0.1,0.1-0.2,0.1-0.3v-0.1 c0-0.1,0-0.2,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0-0.2,0-0.2V9.4c0-0.1-0.1-0.2-0.1-0.3l0,0C11.1,9,11.1,8.9,11,8.8 l0,0c-0.1-0.1-0.1-0.2-0.2-0.2L3.6,0.7C2.8-0.2,1.5-0.2,0.7,0.6z" />
+            </svg>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
 
 export default SidebarList;
