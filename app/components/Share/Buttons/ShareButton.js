04b29cd6590836b7ea2897ef7e39ee802898dd3f
Gadzhimari Kachaev <gadzhimari@gmail.com>
2018-03-02 13:41:50 +0300

    Добавлено открытие окна шаринга в iframe вместо новой вкладки

--- app/components/Share/Buttons/ShareButton.js
+++ app/components/Share/Buttons/ShareButton.js
@@ -4,22 +4,49 @@ import React, { PureComponent } from 'react';
 import { SocialIcons } from '../../Icons';
 import { SOCIAL_TYPES_FOR_ANALITICS } from '../../../lib/constants/social';
 
-export default class ShareButton extends PureComponent {
+import { windowOpen } from '../../../lib/window';
+
+class ShareButton extends PureComponent {
   static propTypes = {
     href: PropTypes.string,
     provider: PropTypes.string,
     className: PropTypes.string,
     onClick: PropTypes.func,
-  }
+    openedWindow: PropTypes.bool,
+    windowWidth: PropTypes.number,
+    windowHeight: PropTypes.number,
+  };
+
+  static defaultProps = {
+    openedWindow: true,
+    windowWidth: 550,
+    windowHeight: 400,
+  };
 
   onClick = (e) => {
-    const { provider, onClick } = this.props;
+    const { provider, href, onClick, openedWindow } = this.props;
+
+    e.preventDefault();
+
+    if (openedWindow) {
+      this.openWindow(href);
+    }
 
     if (onClick) {
       onClick(e, {
         socialCode: SOCIAL_TYPES_FOR_ANALITICS[provider],
       });
     }
+  }
+
+  openWindow = (link) => {
+    const { windowHeight, windowWidth } = this.props;
+    const windowOptions = {
+      height: windowHeight,
+      width: windowWidth,
+    };
+
+    windowOpen(link, windowOptions);
   }
 
   render() {
@@ -38,3 +65,5 @@ export default class ShareButton extends PureComponent {
     );
   }
 }
+
+export default ShareButton;
--- app/lib/window.js
+++ app/lib/window.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+import isInternetExplorerBefore from './browser';
 
 /**
  * Проверяет входит ли target в потомки element или является ли он им
@@ -8,4 +9,37 @@
  */
 export function isClickOutside(element, target) {
   return element !== target && !element.contains(target);
+}
+
+export function windowOpen(url, { name, width = 550, height = 400 }) {
+  /* eslint-disable no-mixed-operators */
+  const left = (window.outerWidth / 2)
+    + (window.screenX || window.screenLeft || 0) - (width / 2);
+  const top = (window.outerHeight / 2)
+    + (window.screenY || window.screenTop || 0) - (height / 2);
+  /* eslint-enable no-mixed-operators */
+
+  const config = {
+    height,
+    width,
+    left,
+    top,
+    location: 'no',
+    toolbar: 'no',
+    status: 'no',
+    directories: 'no',
+    menubar: 'no',
+    scrollbars: 'yes',
+    resizable: 'no',
+    centerscreen: 'yes',
+    chrome: 'yes',
+  };
+
+  const shareDialog = window.open(
+    url,
+    isInternetExplorerBefore(10) ? '' : name,
+    Object.keys(config).map(key => `${key}=${config[key]}`).join(', '),
+  );
+
+  return shareDialog;
 }
