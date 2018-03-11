617ac265f542aabf2679c2729c634583a0ccd46b
Голубков Павел <golubkov.p.d@gmail.com>
2018-03-11 14:52:47 +0500

    дополнительные заголовки

--- server/middlewares/setupAccessHeader.js
+++ server/middlewares/setupAccessHeader.js
@@ -1,4 +1,9 @@
 export default function setupAccessHeader(req, res, next) {
+  res.set(
+    'Access-Control-Allow-Headers',
+    'accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with'
+  );
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.set('Access-Control-Allow-Origin', '*');
 
   next();
