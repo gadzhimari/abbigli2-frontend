a759c15ef7fddac8d0f9318cfd706be91c17d2df
Голубков Павел <golubkov.p.d@gmail.com>
2018-03-11 13:27:12 +0500

    Добавил redux-actions для Support

--- app/ducks/Support.js
+++ app/ducks/Support.js
@@ -1,44 +1,40 @@
-import { createAction } from 'redux-actions';
+import { createActions, handleActions } from 'redux-actions';
 import { Support } from '../api';
 import { openPopup } from './Popup/actions';
 
-export const SUPPORT_REQUEST = 'SUPPORT_REQUEST';
-export const SUPPORT_SUCCESS = 'SUPPORT_SUCCESS';
-export const SUPPORT_FAILURE = 'SUPPORT_FAILURE';
-
-const requestSupport = createAction(SUPPORT_REQUEST);
-const receiveSupport = createAction(SUPPORT_SUCCESS);
-const supportError = createAction(SUPPORT_FAILURE);
+const {
+  supportRequest,
+  supportSuccess,
+  supportFailure
+} = createActions('SUPPORT_REQUEST', 'SUPPORT_SUCCESS', 'SUPPORT_FAILURE');
 
 const initalState = {
   isFetching: false,
   errors: {},
 };
 
-export default function support(state = initalState, action) {
-  switch (action.type) {
-    case SUPPORT_REQUEST:
-      return {
-        ...state,
-        isFetching: true,
-        errors: {},
-      };
-    case SUPPORT_SUCCESS:
-      return {
-        ...state,
-        isFetching: false,
-        errors: {},
-      };
-    case SUPPORT_FAILURE:
-      return {
-        ...state,
-        isFetching: false,
-        errors: action.payload,
-      };
-    default:
-      return state;
+export default handleActions({
+  [supportRequest](state) {
+    return {
+      ...state,
+      isFetching: true,
+      errors: {}
+    };
+  },
+  [supportSuccess](state) {
+    return {
+      ...state,
+      isFetching: false
+    };
+  },
+  [supportFailure](state, payload) {
+    return {
+      ...state,
+      isFetching: false,
+      errors: payload
+    };
   }
-}
+}, initalState);
 
 export function getSupport(data) {
   const formData = new FormData();
@@ -49,17 +45,17 @@ export function getSupport(data) {
   formData.append('file', data.file);
 
   return (dispatch) => {
-    dispatch(requestSupport());
+    dispatch(supportRequest());
 
     return Support.sendMessage(formData)
       .then(() => {
-        dispatch(receiveSupport());
+        dispatch(supportSuccess());
         dispatch(openPopup('statusPopup', {
           title: 'Message have been successfully sent',
         }));
       })
       .catch((err) => {
-        dispatch(supportError(err.response.data));
+        dispatch(supportFailure(err.response.data));
       });
   };
 }
