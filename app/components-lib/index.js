7eebab5f34091f81919ef34b4b432d776c772ac8
Gadzhimari Kachaev <gadzhimari@gmail.com>
2018-03-22 12:31:40 +0300

    Удалены комменты при ребейзе

--- app/components-lib/index.js
+++ app/components-lib/index.js
@@ -1,10 +1,7 @@
 export { default as Like } from './Like';
 export { default as City } from './City';
 export { default as Spin } from './Spin';
-<<<<<<< HEAD
 export { default as Collapse } from './Collapse';
 export { default as ResizeSensor } from './ResizeSensor';
 export { default as ReadMore } from './ReadMore';
-=======
 export { default as NotFound } from './NotFound';
->>>>>>> Добавлена верстка страницы 404
