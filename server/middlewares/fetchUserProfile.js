1d9370422a4fef359c45532e37776b8edd3a2b36
Голубков Павел <golubkov.p.d@gmail.com>
2018-03-22 22:50:10 +0500

    Убрал консоль лог

--- server/middlewares/fetchUserProfile.js
+++ server/middlewares/fetchUserProfile.js
@@ -3,8 +3,6 @@ import { fetchMe } from '../../app/ducks/Auth/authActions';
 export default function fetchUserProfile(req, res, next) {
   const { redux, cookies } = req;
 
-  console.log(cookies.id_token);
-
   if (!cookies.id_token) {
     next();
     return;
