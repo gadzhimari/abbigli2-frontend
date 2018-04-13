da9d4615220c0c64cee61c8fede6561f1b4b26df
Gadzhimari Kachaev <gadzhimari@gmail.com>
2018-04-13 10:38:46 +0300

    Заменена иконка удаления в карточках

--- app/icons/close.js
+++ app/icons/close.js
@@ -1,6 +1,6 @@
 
 import createIconComponent from './utils/createIconComponent';
 import React from 'react';
-const IconClose = createIconComponent({ content: <g><path d="M10.7 8l4.7-4.7c.7-.7.7-2 0-2.7-.7-.7-2-.7-2.7 0L8 5.3 3.3.6c-.7-.7-2-.7-2.7 0s-.7 2 0 2.7L5.3 8 .6 12.7c-.7.7-.7 2 0 2.7s2 .7 2.7 0L8 10.7l4.7 4.7c.7.7 2 .7 2.7 0 .7-.7.7-2 0-2.7L10.7 8z"/></g>, height: 16, width: 16 });
+const IconClose = createIconComponent({ content: <g><path d="M16 1.6L14.4 0 8 6.4 1.6 0 0 1.6 6.4 8 0 14.4 1.6 16 8 9.6l6.4 6.4 1.6-1.6L9.6 8 16 1.6z"/></g>, height: 16, width: 16 });
 IconClose.displayName = 'IconClose';
 export default IconClose;
