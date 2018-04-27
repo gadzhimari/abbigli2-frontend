d4dbbf6a12801ac52f10f709437c1b6e63bf1fa4
Голубков Павел <golubkov.p.d@gmail.com>
2018-04-27 22:04:12 +0500

    Новые иконки в деле

--- app/containers/Profile/ProfileSubMenu.js
+++ app/containers/Profile/ProfileSubMenu.js
@@ -3,6 +3,7 @@ import Type from 'prop-types';
 import classNames from 'classnames';
 
 import Link from '../../components/Link/Link';
+import { Icon } from '../../components-lib';
 
 import { gaSendClickEvent } from '../../lib/analitics';
 import { __t } from '../../i18n/translator';
@@ -25,8 +26,9 @@ export default class ProfileSubMenu extends PureComponent {
   render() {
     const { data, isMe, path } = this.props;
 
-    const { is_favorite_visible: showFavorites,
-            is_feed_visible: showFeed,
+    const {
+      is_favorite_visible: showFavorites,
+      is_feed_visible: showFeed,
     } = data;
 
     const commonClass = 'profile-submenu__item';
@@ -65,9 +67,7 @@ export default class ProfileSubMenu extends PureComponent {
             name="favorites"
           >
             <div className="icon-wrap">
-              <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 31.193">
-                <path d="M17,31.193l-2.467-2.242C5.778,21.011,0,15.774,0,9.35C0,4.113,4.113,0,9.351,0C12.308,0,15.147,1.377,17,3.552 C18.853,1.377,21.691,0,24.649,0C29.886,0,34,4.113,34,9.35c0,6.425-5.781,11.661-14.537,19.618L17,31.193z" />
-              </svg>
+              <Icon glyph="heart" />
             </div>
             {__t('Favorites')}
           </Link>
@@ -83,9 +83,7 @@ export default class ProfileSubMenu extends PureComponent {
             name="feed"
           >
             <div className="icon-wrap">
-              <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
-                <path d="M6,10V5h4v5H6z M6,0h4v3H6V0z M0,7h4v3H0V7z M0,0h4v5H0V0z" />
-              </svg>
+              <Icon glyph="feed" />
             </div>
             {__t('Feed')}
           </Link>
@@ -101,9 +99,7 @@ export default class ProfileSubMenu extends PureComponent {
             name="messages"
           >
             <div className="icon-wrap">
-              <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 8">
-                <path d="M9,0H1C0.45,0,0.005,0.449,0.005,1L0,7c0,0.55,0.45,1,1,1h8c0.55,0,1-0.45,1-1V1C10,0.449,9.55,0,9,0z M9,2 L5,4.5L1,2V1l4,2.5L9,1V2z" />
-              </svg>
+              <Icon glyph="mail" />
             </div>
             {__t('Messages')}
           </Link>
@@ -118,12 +114,7 @@ export default class ProfileSubMenu extends PureComponent {
           name="about_me"
         >
           <div className="icon-wrap">
-            <svg className="icon" viewBox="0 0 22.2 25.6">
-              <g id="XMLID_44_">
-                <path id="XMLID_61_" className="st0" d="M11.1,14C6.6,14,2.6,15.3,0,17.3c1.3,4.8,5.8,8.3,11.1,8.3c5.3,0,9.8-3.5,11.1-8.3 C19.6,15.3,15.6,14,11.1,14z" />
-                <circle id="XMLID_77_" className="st0" cx="11.1" cy="6.3" r="6.3" />
-              </g>
-            </svg>
+            <Icon glyph="user" />
           </div>
           {__t('About me')}
         </Link>
