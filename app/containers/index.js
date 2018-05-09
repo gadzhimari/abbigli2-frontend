cba2ccee79b17210b73a31e89c926da151e25f9a
Голубков Павел <golubkov.p.d@gmail.com>
2018-05-09 21:15:44 +0500

    Правки ошибок слияния

--- app/containers/index.js
+++ app/containers/index.js
@@ -24,5 +24,3 @@ export { default as PeopleSearch } from './PeopleSearch';
 export { default as ProfileEvents } from './ProfileEvents';
 export { default as ProfileBlogs } from './ProfileBlogs';
 export { default as ForMasters } from './ForMasters';
-export { default as ProfileEvents } from './ProfileEvents';
-export { default as ProfileBlogs } from './ProfileBlogs';
