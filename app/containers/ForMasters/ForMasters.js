7b3572348ff2ab0e1ddc2cad36f76d50f807d92a
Голубков Павел <golubkov.p.d@gmail.com>
2018-05-27 15:16:11 +0500

    Правка аналитики

--- app/containers/ForMasters/ForMasters.js
+++ app/containers/ForMasters/ForMasters.js
@@ -6,6 +6,7 @@ import IconPlus from '../../icons/plus';
 import { stagedPopup } from '../../ducks/Auth/authActions';
 
 import pages from '../../lib/pages';
+import { gaSendClickEvent } from '../../lib/analitics';
 import { __t } from '../../i18n/translator';
 
 import './ForMasters.less';
@@ -22,17 +23,18 @@ class ForMasters extends Component {
   }
 
   handleOpenCreating = () => {
-    const {
-      isAuthenticated,
-      router,
-      showRegister
-    } = this.props;
+    const { isAuthenticated, router, showRegister } = this.props;
+    let event;
 
     if (isAuthenticated) {
+      event = 'create';
       router.push(pages.CREATE_PAGE.path);
     } else {
+      event = 'join';
       showRegister();
     }
+
+    gaSendClickEvent('lp', event);
   }
 
   render(cn) {
