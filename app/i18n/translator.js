dbbb4fb7c27cf60ad077487325edd6b7052dca3c
Голубков Павел <golubkov.p.d@gmail.com>
2018-06-23 13:04:43 +0500

    Намётки plural

--- app/i18n/translator.js
+++ app/i18n/translator.js
@@ -11,6 +11,10 @@ const langs = { en, ru };
 export const __t = (key, params) => {
   let result = langs[location][key] || key;
 
+  // if (isObject(result)) {
+  //   result = plural(result, params);
+  // }
+
   if (isObject(params)) {
     forIn(params, (value, key) => {
       result = result.replace(`{{${key}}}`, value);
@@ -19,3 +23,42 @@ export const __t = (key, params) => {
 
   return result;
 };
+
+// const plural = (translate, params = {}) => {
+//   let text = translate.text;
+
+//   forIn(params, (value, key) => {
+//     const pluralForParam = translate[key];
+
+//     if (pluralForParam) {
+//       text = text.replace(`{{${key}}}`, value);
+//     } else {
+//       const pluralCategory = getPluralCategory(value);
+//     }
+//   });
+
+//   return text;
+// };
+
+// const getPluralCategory = (v) => {
+//   const restOfTen = v % 10;
+//   const restOfHundred = v % 100;
+
+//   const isOne = restOfTen === 1 && restOfHundred !== 11;
+//   const isFew = restOfTen === 0 || (restOfTen >= 2 && restOfTen <= 4 &&
+//     restOfHundred < 12 && restOfHundred > 14);
+//   const isMany = restOfTen === 0 || (restOfTen >= 5 && restOfTen <= 9) ||
+//     (restOfTen >= 11 && restOfTen <= 14);
+
+//   if (isOne) {
+//     return 'one';
+//   }
+
+//   if (isFew) {
+//     return 'few';
+//   }
+
+//   if (isMany) {
+//     return 'many';
+//   }
+// };
